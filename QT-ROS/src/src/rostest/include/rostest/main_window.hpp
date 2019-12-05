b288b21e87b657331714f614a76460423468590c
王帅 <38455672+ShuaiWang-Code@users.noreply.github.com>
2019-12-05 23:41:31 +0800

    Add files via upload

---
+++ QT-ROS/src/src/rostest/include/rostest/main_window.hpp
@@ -0,0 +1,74 @@
+/**
+ * @file /include/rostest/main_window.hpp
+ *
+ * @brief Qt based gui for rostest.
+ *
+ * @date November 2010
+ **/
+#ifndef rostest_MAIN_WINDOW_H
+#define rostest_MAIN_WINDOW_H
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QtGui/QMainWindow>
+#include "ui_main_window.h"
+#include "qnode.hpp"
+
+/*****************************************************************************
+** Namespace
+*****************************************************************************/
+
+namespace rostest {
+
+/*****************************************************************************
+** Interface [MainWindow]
+*****************************************************************************/
+/**
+ * @brief Qt central, all operations relating to the view part here.
+ */
+class MainWindow : public QMainWindow {
+Q_OBJECT
+
+public:
+	MainWindow(int argc, char** argv, QWidget *parent = 0);
+	~MainWindow();
+
+	void ReadSettings(); // Load up qt program settings at startup
+	void WriteSettings(); // Save qt program settings when closing
+
+	void closeEvent(QCloseEvent *event); // Overloaded function
+	void showNoMasterMessage();
+
+public Q_SLOTS:
+	/******************************************
+	** Auto-connections (connectSlotsByName())
+	*******************************************/
+	void on_actionAbout_triggered();
+	void on_button_connect_clicked(bool check );
+	void on_checkbox_use_environment_stateChanged(int state);
+
+    /******************************************
+    ** Manual connections
+    *******************************************/
+    void updateLoggingView(); // no idea why this can't connect automatically
+
+    private Q_SLOTS:
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+private:
+	Ui::MainWindowDesign ui;
+	QNode qnode;
+};
+
+}  // namespace rostest
+
+#endif // rostest_MAIN_WINDOW_H
